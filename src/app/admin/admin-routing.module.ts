a5738eb2317033e988f518bc5d6d55b97ec4bbe1
David <dheyved1@gmail.com>
2023-01-10 16:23:37 +0100

    display products UI

--- src/app/admin/admin-routing.module.ts
+++ src/app/admin/admin-routing.module.ts
@@ -2,7 +2,9 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AdminComponent } from './admin.component';
 import { AdminOverviewComponent } from './components/admin-overview/admin-overview.component';
+import { AddAdminProductsComponent } from './components/admin-products/add-admin-products/add-admin-products.component';
 import { DisplayAdminProductsComponent } from './components/admin-products/display-admin-products/display-admin-products.component';
+import { EditAdminProductsComponent } from './components/admin-products/edit-admin-products/edit-admin-products.component';
 import { AdminSettingsComponent } from './components/admin-settings/admin-settings.component';
 
 const routes: Routes = [
@@ -23,6 +25,22 @@ const routes: Routes = [
         component: DisplayAdminProductsComponent,
         data: {
           title: 'Products',
+          description: 'Description Meta Tag Content',
+        },
+      },
+      {
+        path: 'products/add-product',
+        component: AddAdminProductsComponent,
+        data: {
+          title: 'Add Product',
+          description: 'Description Meta Tag Content',
+        },
+      },
+      {
+        path: 'products/edit-product/:productId',
+        component: EditAdminProductsComponent,
+        data: {
+          title: 'Edit Product',
           description: 'Description Meta Tag Content',
         },
       },
--- src/app/admin/components/admin-products/display-admin-products/display-admin-products.component.ts
+++ src/app/admin/components/admin-products/display-admin-products/display-admin-products.component.ts
@@ -7,4 +7,67 @@ import { Component } from '@angular/core';
 })
 export class DisplayAdminProductsComponent {
 
+  products = [
+    {
+      id: 1,
+      name: 'White marble top side table with T shaped base',
+      description: 'Description',
+      image: 'assets/img/wall-deco.jpg',
+      categories: ['Curtain', 'Rug', 'Towel'],
+      dateAdded: '20/10/22',
+      lastUpdated: '02/01/23',
+      status: 'available',
+      price: 2000,
+      totalQuantity: 4
+    },
+    {
+      id: 2,
+      name: 'Lighting',
+      description: 'Description',
+      image: 'assets/img/lighthing.jpg',
+      categories: ['Curtain', 'Rug', 'Towel'],
+      dateAdded: '20/10/22',
+      lastUpdated: '02/01/23',
+      status: 'available',
+      price: 1000,
+      totalQuantity: 4
+    },
+    {
+      id: 3,
+      name: 'Vase of flowers',
+      description: 'Description',
+      image: 'assets/img/vase-flowers.jpg',
+      categories: ['Curtain', 'Rug', 'Towel'],
+      dateAdded: '20/10/22',
+      lastUpdated: '02/01/23',
+      status: 'available',
+      price: 3000,
+      totalQuantity: 4
+    },
+    {
+      id: 4,
+      name: 'Throw pillows',
+      description: 'Description',
+      image: 'assets/img/throw-pillows.jpg',
+      categories: ['Curtain', 'Rug', 'Towel'],
+      dateAdded: '20/10/22',
+      lastUpdated: '02/01/23',
+      status: 'available',
+      price: 500,
+      totalQuantity: 4
+    },
+  ]
+
+  dataLoading: boolean = true;
+
+  editProduct(product: any) {
+
+  }
+  deleteProduct(product: any) {
+
+  }
+  updateProductStatus(product: any) {
+
+  }
+
 }
