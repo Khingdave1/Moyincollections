2aea6653ec67f29c20085ad7e4d98f09e6449c0e
David <dheyved1@gmail.com>
2023-01-23 22:16:49 +0100

    update gitignore

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+    production: true,
+    firebase: {
+    projectId: 'moheen-collections',
+    appId: '1:555842081099:web:d0c0ff520c787dbc2818ce',
+    databaseURL: 'https://moheen-collections-default-rtdb.firebaseio.com',
+    storageBucket: 'moheen-collections.appspot.com',
+    apiKey: 'AIzaSyAhJj6azeyWhaiZLONCeICfHtM2rCnDzDk',
+    authDomain: 'moheen-collections.firebaseapp.com',
+    messagingSenderId: '555842081099',
+    measurementId: 'G-4DKN67721F',
+    },
+};
